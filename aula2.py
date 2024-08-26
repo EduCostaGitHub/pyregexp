c93942cdb292cdd6769e3b69d74846df9eb8b9b2
Eduardo Costa <educosta.costa@gmail.com>
2024-08-26 15:48:31 +0100

    metacaracteres

--- aula2.py
+++ aula2.py
@@ -8,8 +8,6 @@
 
 import re
 
-
-
 text = """
 João trouxe    flores para sua amada namorada em 10 de janeiro de 1970,
 Maria era o nome dela.
@@ -21,5 +19,12 @@ domingo. Também né! Sendo a boa mineira que é, nunca esquece seu famoso
 pão de queijo.
 Não canso de ouvir a Maria:
 "Joooooooooãooooooo, o café tá prontinho aqui. Veeemm"!
-
 """
+print(re.findall('João|Maria',text))
+print(re.findall('João|Maria|ad.ltos',text)) # . Qualquer coisa
+print(re.findall('[Jj]oão|[Mm]aria',text)) # J ou j
+print(re.findall('[a-z]aria',text)) # Qualquer letra de a-z
+print(re.findall('[a-zA-Z]aria',text)) # Qualquer letra de a-z ou de A-Z
+print(re.findall('[a-zA-Z0-9]aria',text)) # Qualquer letra de a-z ou de A-Z ou 0-9
+print(re.findall('[a-zA-Z0-9]oão',text)) # Qualquer letra de a-z ou de A-Z ou 0-9
+print(re.findall('joÃO| mAriA',text, flags=re.IGNORECASE)) # flag Ignore cae
