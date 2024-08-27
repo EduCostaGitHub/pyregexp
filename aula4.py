19a49df51e942f0fd2ab85a4ef3a10be0ad5330f
Eduardo Costa <educosta.costa@gmail.com>
2024-08-27 16:36:45 +0100

    Greey e non-greedy

--- aula4.py
+++ aula4.py
@@ -1,24 +1,14 @@
 # https://docs.python.org/3/library/re.html
 # https://docs.python.org/3/howto/regex.html#regex-howto
 
-# Meta caracteres: . ^ $ * + ? { } [ ] \ | ( )
-# | OU
-# . Qualquer caractere (com exceção da quebra de linha)
-# [] conjunto de caracteres
-
 # Meta caracteres: ^ $ ( )
 # * 0 ou n
 # + 1 ou n {1,}
 # ? 0 ou 1
-# {n}
-# {min, max}
-# {10,} 10 ou mais
-# {,10} De zero a 10
-# {10} Especificamente 10
-# {5,10} De 5 a 10
-# ()+ [a-zA-Z0-9]+
 
 import re
+
+text = """<p> Phrase 1 </p><p> Phrase 2 </p><p> Phrase 3 </p><div> Phrase 4 </div>"""
 
 
 
