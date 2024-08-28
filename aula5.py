0d86d7c6e731a768dfef5ffa40bb6ae0974fc8e6
Eduardo Costa <educosta.costa@gmail.com>
2024-08-28 21:18:31 +0100

    Grupos e Retrovisores

---
+++ aula5.py
@@ -0,0 +1,9 @@
+# https://docs.python.org/3/library/re.html
+# https://docs.python.org/3/howto/regex.html#regex-howto
+
+# Meta caracteres: ^ $ ( )
+# * 0 ou n
+# + 1 ou n {1,}
+# ? 0 ou 1
+
+import re
