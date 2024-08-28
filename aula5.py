30d7c981d1d1f486e54140baf07bff243100377b
Eduardo Costa <educosta.costa@gmail.com>
2024-08-28 22:08:16 +0100

    Grupos e Retrovisores

--- aula5.py
+++ aula5.py
@@ -6,4 +6,22 @@
 # + 1 ou n {1,}
 # ? 0 ou 1
 
+# ()    \1
+# () () \1 \2
+# (())() \1 \2 \3
+
 import re
+from pprint import pprint
+
+
+text = """
+<p> Phrase 1 </p><p> Phrase 2 </p><p> Phrase 3 </p><div> Phrase 4 </div>
+"""
+print(re.findall(r'<[dpiv]{1,3}>.*?<\/[dpiv]{1,3}>', text))  # non greedy /lazy
+print()
+tags = re.findall(r'(<([dpiv]{1,3})>.*?<\/\2>)', text)  # non greedy /lazy
+
+for tag in tags:
+    um, dois = tag
+    print(um)
+    print(dois)
